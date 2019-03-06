c15cfc561431bc9f6f5fab0c69bb499b9e738a7b
Mariland2 <45314516+Mariland2@users.noreply.github.com>
2019-03-06 18:34:26 +0300

    4 задача (1 УРОК)

---
+++ 4.py
@@ -0,0 +1,42 @@
+from flask import Flask, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "<h1>Привет, яндекс! Я - Кирилл</h1>"
+
+@app.route('/image_sample')
+def image_sample():
+        return '''<img src="{}" alt="здесь должна была быть картинка, 
+        но не нашлась">'''.format(url_for('static', filename='img/1.jpeg'))
+
+
+@app.route('/bootstrap_sample')
+def bootstrap():
+    return '''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width,
+                    initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet"
+                    href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+                    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+                    crossorigin="anonymous">
+                    <title>Привет, Яндекс!</title>
+                  </head>
+                  <body>
+                    <h1>Привет, Яндекс!</h1>
+                    <div class="alert alert-primary" role="alert">
+                      А мы тут компонентами Bootstrap балуемся
+                    </div>
+                  </body>
+                </html>'''
+
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
